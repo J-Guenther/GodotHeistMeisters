93b5b5f5765af0f1e728ae48d191c9b114302dea
j-guenther <jannik.guentheg@gmail.com>
2019-01-05 22:03:39 +0100

    added doors and locked doors

--- HeistMeisters/Scripts/Global.gd
+++ HeistMeisters/Scripts/Global.gd
@@ -7,3 +7,6 @@ var destinations
 # Asset Links - MUST BE CHANGED MANUALLY
 var nightvision_on_sfx = "res://SFX/nightvision.wav"
 var nightvision_off_sfx = "res://SFX/nightvision_off.wav"
+
+var blue_light = "res://GFX/Interface/PNG/dotBlue.png"
+var green_light = "res://GFX/Interface/PNG/dotGreen.png"
---
+++ HeistMeisters/Scripts/NumberPad.gd
@@ -0,0 +1,56 @@
+extends Popup
+
+var combination = []
+var guess = []
+
+signal combination_correct
+
+onready var display = $VSplitContainer/DisplayContainer/Display
+onready var light = $VSplitContainer/ButtonContainer/ButtonGrid/Light
+
+func _ready():
+	connect_buttons()
+	reset_lock()
+
+func connect_buttons():
+	for child in $VSplitContainer/ButtonContainer/ButtonGrid.get_children():
+		if child is Button:
+			child.connect("pressed", self, "_on_Button_pressed", [child.text])
+
+func _on_Button_pressed(button):
+	if button == "Ok":
+		check_guess()
+	else:
+		enter(int(button))
+
+
+func check_guess():
+	if guess == combination:
+		light.texture = load(Global.green_light)
+		$AudioStreamPlayer.stream = load("res://SFX/threeTone1.ogg")
+		$AudioStreamPlayer.play()
+		$Timer.start()
+	else:
+		reset_lock()
+
+func enter(button):
+	$AudioStreamPlayer.stream = load("res://SFX/twoTone1.ogg")
+	$AudioStreamPlayer.play()
+	guess.append(button)
+	update_display()
+
+
+func reset_lock():
+	light.texture = load(Global.blue_light)
+	display.clear()
+	guess.clear()
+
+func update_display():
+	display.bbcode_text = "[center]" + PoolStringArray(guess).join("") + "[/center]"
+	if guess.size() == combination.size():
+		check_guess()
+
+func _on_Timer_timeout():
+	emit_signal("combination_correct")
+	hide()
+	reset_lock()
